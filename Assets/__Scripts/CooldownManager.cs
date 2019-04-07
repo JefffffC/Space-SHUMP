444d948264efae3a1b77e7105a92becac52020d2
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2019-04-07 18:11:20 -0400

    hotfix to commit overwrite issue
    Also added cooldown UI objects for implementation of certain powerups

---
+++ Assets/__Scripts/CooldownManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class CooldownManager : MonoBehaviour
+{
+    [Header("Set in the Inspector")]
+    public GameObject coolDownBox;
+    public bool coolingDown = false;
+    public float cooldownInvincible = 10.0f;
+    public float cooldownRandomWeapon = 30.0f;
+
+    [Header("Set Dynamically")]
+    [SerializeField] private WeaponType wt;
+    [SerializeField] private Color powerupColor;
+    [SerializeField] private Image coolDownBar;
+    [SerializeField] private Text powerupLabel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolingDown == true)
+        {
+            //Reduce fill amount over 30 seconds
+            .fillAmount -= 1.0f / cooldownInvincible * Time.deltaTime;
+        }
+    }
+}
--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -15,6 +15,7 @@ public class Hero : MonoBehaviour
     public float gameRestartDelay = 2f;
     public GameObject projectilePrefab;
     public float projectileSpeed = 40;
+    public Weapon activeWeapon;
 
   
 
